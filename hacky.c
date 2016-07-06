4194bca9b4a43f1dbd78ffd7d2fab67b88516241
magniff <tinysnippets@gmail.com>
2016-07-06 11:20:35 -0400

    Add dict_fetcher and update .gitignore file.

--- hacky.c
+++ hacky.c
@@ -1,7 +1,9 @@
 #include <Python.h>
+#include <frameobject.h>
+#include <pystate.h>
 
 
-static PyObject*
+PyObject*
 set_class(PyObject *self, PyObject *args)
 {
     PyObject *obj;
@@ -45,12 +47,23 @@ write_memory_in(PyObject *self, PyObject *args)
 }
 
 
+PyObject *
+run_frame(PyObject *self, PyObject *args)
+{
+    PyThreadState *current_state = PyThreadState_Get(); 
+    PyFrameObject *f;
+    if (!PyArg_UnpackTuple(args, "run_frame", 1, 1, &f))
+        return NULL;
+    current_state->frame = f;
+    f->f_back=f;
+    return PyEval_EvalFrame(f);
+}
+
 
 PyObject *
 get_flags(PyObject *self, PyObject *args)
 {
     PyTypeObject *cls;
-
     if (!PyArg_UnpackTuple(args, "get_flags", 1, 1, &cls))
         return NULL;
 
@@ -73,26 +86,48 @@ set_flags(PyObject *self, PyObject *args)
 }
 
 
+PyObject *
+fetch_dict(PyObject *self, PyObject *args)
+{
+    PyTypeObject *type;
+    PyObject *dictobject;
+    if (!PyArg_UnpackTuple(args, "fetch_dict", 1, 1, &type))
+        return NULL;
+    dictobject = type->tp_dict;
+    Py_INCREF(dictobject);
+    return dictobject;
+}
+
+
+
 static PyMethodDef HackyMethods[] = {
     {
-        "set_class",  set_class,
+        "set_class", set_class,
         METH_VARARGS, "Sets type."
     },
     {
-        "get_flags",  get_flags,
+        "get_flags", get_flags,
         METH_VARARGS, "Get type`s flags."
     },
     {
-        "read_memory_in",  read_memory_in,
+        "read_memory_in", read_memory_in,
         METH_VARARGS, "Get type`s flags."
     },
     {
-        "write_memory_in",  write_memory_in,
+        "write_memory_in", write_memory_in,
         METH_VARARGS, "Get type`s flags."
     },
     {
-        "set_flags",  set_flags,
+        "set_flags", set_flags,
         METH_VARARGS, "Set type`s flags."
+    },
+    {
+        "run_frame", run_frame,
+        METH_VARARGS, "Evals frame."
+    },
+    {
+        "fetch_dict", fetch_dict,
+        METH_VARARGS, "Returns actual type`s dictionary."
     },
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
@@ -102,7 +137,7 @@ static struct PyModuleDef hackymodule = {
     PyModuleDef_HEAD_INIT,
     "hacky",  /* name of module */
     0,  /* module documentation, may be NULL */
-   -1,  /* size of per-interpreter state of the module,
+    0,  /* size of per-interpreter state of the module,
            or -1 if the module keeps state in global variables. */
    HackyMethods
 };
