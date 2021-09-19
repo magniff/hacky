34d07f1a3e97e6fd545cad6993d0446392ebbe54
magniff <tinysnippets@gmail.com>
2021-09-19 14:19:19 +0300

    Autoformat.

--- hacky.c
+++ hacky.c
@@ -2,8 +2,7 @@
 #include <frameobject.h>
 #include <pystate.h>
 
-
-PyObject*
+PyObject *
 set_class(PyObject *self, PyObject *args)
 {
     PyObject *obj;
@@ -16,7 +15,6 @@ set_class(PyObject *self, PyObject *args)
     Py_RETURN_NONE;
 }
 
-
 PyObject *
 read_memory_in(PyObject *self, PyObject *args)
 {
@@ -28,7 +26,6 @@ read_memory_in(PyObject *self, PyObject *args)
     return PyLong_FromUnsignedLong(*((unsigned char *)address));
 }
 
-
 PyObject *
 write_memory_in(PyObject *self, PyObject *args)
 {
@@ -37,28 +34,26 @@ write_memory_in(PyObject *self, PyObject *args)
     if (!PyArg_UnpackTuple(args, "write_memory_in", 2, 2, &address_object, &new_data_object))
         return NULL;
 
-    unsigned char *address = \
+    unsigned char *address =
         (unsigned char *)PyLong_AsVoidPtr(address_object);
-    unsigned char new_data = \
+    unsigned char new_data =
         (unsigned char)PyLong_AsUnsignedLong(new_data_object);
 
     *address = new_data;
     Py_RETURN_NONE;
 }
 
-
 PyObject *
 run_frame(PyObject *self, PyObject *args)
 {
-    PyThreadState *current_state = PyThreadState_Get(); 
+    PyThreadState *current_state = PyThreadState_Get();
     PyFrameObject *f;
     if (!PyArg_UnpackTuple(args, "run_frame", 1, 1, &f))
         return NULL;
     current_state->frame = f;
-    f->f_back=f;
+    f->f_back = f;
     return PyEval_EvalFrame(f);
 }
-
 
 PyObject *
 get_flags(PyObject *self, PyObject *args)
@@ -69,7 +64,6 @@ get_flags(PyObject *self, PyObject *args)
 
     return PyLong_FromUnsignedLong(cls->tp_flags);
 }
-
 
 PyObject *
 set_flags(PyObject *self, PyObject *args)
@@ -85,7 +79,6 @@ set_flags(PyObject *self, PyObject *args)
     Py_RETURN_NONE;
 }
 
-
 PyObject *
 fetch_dict(PyObject *self, PyObject *args)
 {
@@ -98,50 +91,31 @@ fetch_dict(PyObject *self, PyObject *args)
     return dictobject;
 }
 
-
-
 static PyMethodDef HackyMethods[] = {
-    {
-        "set_class", set_class,
-        METH_VARARGS, "Sets type."
-    },
-    {
-        "get_flags", get_flags,
-        METH_VARARGS, "Get type`s flags."
-    },
-    {
-        "read_memory_in", read_memory_in,
-        METH_VARARGS, "Get type`s flags."
-    },
-    {
-        "write_memory_in", write_memory_in,
-        METH_VARARGS, "Get type`s flags."
-    },
-    {
-        "set_flags", set_flags,
-        METH_VARARGS, "Set type`s flags."
-    },
-    {
-        "run_frame", run_frame,
-        METH_VARARGS, "Evals frame."
-    },
-    {
-        "fetch_dict", fetch_dict,
-        METH_VARARGS, "Returns actual type`s dictionary."
-    },
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+    {"set_class", set_class,
+     METH_VARARGS, "Sets type."},
+    {"get_flags", get_flags,
+     METH_VARARGS, "Get type`s flags."},
+    {"read_memory_in", read_memory_in,
+     METH_VARARGS, "Get type`s flags."},
+    {"write_memory_in", write_memory_in,
+     METH_VARARGS, "Get type`s flags."},
+    {"set_flags", set_flags,
+     METH_VARARGS, "Set type`s flags."},
+    {"run_frame", run_frame,
+     METH_VARARGS, "Evals frame."},
+    {"fetch_dict", fetch_dict,
+     METH_VARARGS, "Returns actual type`s dictionary."},
+    {NULL, NULL, 0, NULL} /* Sentinel */
 };
-
 
 static struct PyModuleDef hackymodule = {
     PyModuleDef_HEAD_INIT,
-    "hacky",  /* name of module */
-    0,  /* module documentation, may be NULL */
-    0,  /* size of per-interpreter state of the module,
+    "hacky", /* name of module */
+    0,       /* module documentation, may be NULL */
+    0,       /* size of per-interpreter state of the module,
            or -1 if the module keeps state in global variables. */
-   HackyMethods
-};
-
+    HackyMethods};
 
 PyMODINIT_FUNC
 PyInit_hacky(void)
